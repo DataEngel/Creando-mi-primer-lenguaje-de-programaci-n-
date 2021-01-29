92434f89c62b6cec0114441c669952450ba21b79
DataEngel <miguel.program.73@gmail.com>
2021-01-28 22:07:01 -0600

    analizador léxico

---
+++ Mi Lenguaje/main.py
@@ -0,0 +1,12 @@
+from lpp.repl import start_repl
+
+
+def main() -> None:
+    print('Bienvenido a mi primer lenguaje de programación.')
+    print('Escribe una instrucción para comenzar') 
+
+    start_repl()
+
+
+if __name__ == '__main__':
+    main()
